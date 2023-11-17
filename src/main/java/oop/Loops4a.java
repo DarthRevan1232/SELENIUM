f99663105f79bbf8d58d2bdb80ab39ca68fca75f
unknown <mreliasz777@gmail.com>
2023-11-17 07:25:21 +0100

    third commit

--- src/main/java/oop/Loops4a.java
+++ src/main/java/oop/Loops4a.java
@@ -1,25 +1,25 @@
 package oop;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
-public class Loops {
+public class Loops4a {
 
     static String[] tablica;    // deklaracja zmiennej
 
     //psvm -> public static void main(String[] args) {
     public static void main(String[] args) {
         tablica = new String[3];   // inicjalizacja zmiennej zadeklarowanej w linii 9
-        String str1 = "Ala ";
-        String str2 = "ma ";
-        String str3 = "kota";
+        String str1 = "10";
+        String str2 = "20";
+        String str3 = "30";
         tablica[0] = str1;
         tablica[1] = str2;
         tablica[2] = str3;
 
 /*
         // pętla while -> wykonywana dopóki warunek = true
+
+ */
         int counter = 1;
         // warunek -> counter <= 10
         while (counter < 10) {
@@ -29,26 +29,28 @@ public class Loops {
             // counter--; -> dekrementacja
         }
 
-          dopóki (warunek jest prawdziwy) {
-            wykonuj kod;
+          //dopóki (warunek jest prawdziwy) {
+            //wykonuj kod;
         }
+for (String l : lista)
 
-    */
 
-        // pętla for
-        //   inicjalizacja ; weryfikacja ; aktualizacja
-        for (int count = 1;  count <= 10 ; count++) {
-            System.out.println("wow! " + count);
-        }
 
-        // pętla for(each) -> iteracja po wszystkich elementach zbioru
-        for (String s : tablica) {
-            // sout ->  System.out.println
-            System.out.println(s);
-        }
 
-        //streamy
 
-        Arrays.stream(tablica).forEach(s -> System.out.println(s));
+
+
+
+
+
+      /*  // pętla for
+        // Pętla na 6
+        for (int i = 0;  i < 100 ; i++) {
+            System.out.println("Liczba:  " + i);
+        } */
+
+
+
     }
-}
+
+
